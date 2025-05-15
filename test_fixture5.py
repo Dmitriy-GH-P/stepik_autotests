062420b1a92e32913c3856775293af3ec497547d
Dima <mandarin666.dr@gmail.com>
2025-05-15 17:40:05 +0300

    Test fixture 5 (marks)

--- test_fixture5.py
+++ test_fixture5.py
@@ -15,12 +15,12 @@ def browser():
 
 
 class TestMainPage1():
-# Маркировки тестов
+# Маркировка Смоук
     @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
-
+# Маркировка регресс
     @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
